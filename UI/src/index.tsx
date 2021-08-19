60b04d64663df0d0bedab5deacc28a079e1a4b15
Chayan <chanyann96@gmail.com>
2021-08-19 15:04:25 +0900

    chore: 불필요한 프로젝트 파일 제거

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reportWebVitals from './reportWebVitals';
+
 import FloatingButton from './Component/FloatingButton';
 import Panel from './Component/Panel';
 
-document.querySelector('meta[property="og:url"]')
-document.querySelector('meta[property="og:image"]')
+document.querySelector('meta[property="og:url"]');
+document.querySelector('meta[property="og:image"]');
 
 ReactDOM.render(
   <React.StrictMode>
-    <Panel/>
+    <Panel />
     <FloatingButton />
   </React.StrictMode>,
   document.getElementById('extension')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
