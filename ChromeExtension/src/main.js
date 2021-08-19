97b0da9f1d236439f0ad254f8b3678e13225dc4b
ribeKim <ribe@ribe.io>
2021-08-19 21:43:28 +0900

    inject 방식 변경

--- ChromeExtension/src/main.js
+++ ChromeExtension/src/main.js
@@ -1,5 +1,8 @@
 
 const element = document.createElement('div');
+const script = document.createElement('script');
 element.id = 'extension'
+script.src = 'chrome-extension://kliibdckegfemgeimfpabobagohdojag/dist/bundle.js'
 
 document.body.append(element);
+document.body.append(script);
