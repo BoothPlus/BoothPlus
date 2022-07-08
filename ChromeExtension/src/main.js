7af9d194366f3ab4917c455db98c48e81f685bc9
ribeKim <ribe@ribe.io>
2022-07-09 02:16:57 +0900

    content.js 로 변경

--- ChromeExtension/src/main.js
+++
@@ -1,8 +0,0 @@
-
-const element = document.createElement('div');
-const script = document.createElement('script');
-element.id = 'extension'
-script.src = 'chrome-extension://kliibdckegfemgeimfpabobagohdojag/dist/bundle.js'
-
-document.body.append(element);
-document.body.append(script);
